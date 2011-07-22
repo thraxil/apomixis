425e6f260c639bf2b47621b19277eea4a7601093
Anders Pearson <anders@columbia.edu>
2011-07-22 11:18:54 -0400

    stripping it down

--- settings_shared.py
+++ settings_shared.py
@@ -8,20 +8,18 @@ ADMINS = ( )
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3' # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ':memory:' # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASE_ENGINE = 'sqlite3' 
+DATABASE_NAME = ':memory:' 
+DATABASE_USER = ''         
+DATABASE_PASSWORD = ''     
+DATABASE_HOST = ''         
+DATABASE_PORT = ''         
 
 TIME_ZONE = 'America/New_York'
 LANGUAGE_CODE = 'en-us'
 SITE_ID = 1
 USE_I18N = False
 MEDIA_ROOT = "/var/www/apomixis/uploads/"
-MEDIA_URL = '/uploads/'
-ADMIN_MEDIA_PREFIX = '/media/'
 SECRET_KEY = 'ASDFGHXCVBERrtdfscvsbwerg'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.load_template_source',
@@ -34,36 +32,25 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     )
 
 MIDDLEWARE_CLASSES = (
-#    'django.middleware.common.CommonMiddleware',
 )
 
 ROOT_URLCONF = 'apomixis.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    # Put application templates before these fallback ones:
     "/var/www/apomixis/templates/",
     os.path.join(os.path.dirname(__file__),"templates"),
 )
 
 INSTALLED_APPS = (
-    'django.contrib.contenttypes',
-    'staticmedia',
     'main',
 )
 
 FILE_UPLOAD_PERMISSIONS = 0644
-THUMBNAIL_SUBDIR = "thumbs"
 EMAIL_SUBJECT_PREFIX = "[apomixis] "
 EMAIL_HOST = 'localhost'
 SERVER_EMAIL = "apomixis@ccnmtl.columbia.edu"
 
-# put any static media here to override app served static media
-STATICMEDIA_MOUNTS = (
-    ('/sitemedia', 'sitemedia'),
-)
+
 
 
 
